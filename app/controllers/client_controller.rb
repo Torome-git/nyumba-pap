2570effb499fdacde8b82b3bb6e7cf6cd91104fc
soila.torome <soila.torome@student.moringaschool.com>
2022-12-06 22:02:44 +0300

    add routing for client

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,2 +1,11 @@
 class ClientController < ApplicationController
+    def index
+        clients = Client.all
+        render json: clients
+    end
+
+    def show
+        client = Client.find_by(id: params[:id])
+        render json: client
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :apartments, only: [:index]
-  
+  resources :client, only: [:index, :show] 
 end
