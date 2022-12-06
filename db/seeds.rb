e5ca7199f7852b569386e0062742b09d07d914d8
soila.torome <soila.torome@student.moringaschool.com>
2022-12-06 21:46:43 +0300

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Deleting old data"
+Client.destroy_all
+Apartment.destroy_all
+Landlord.destroy_all
+
+puts "🌱 Seeding data..."
+
+puts "seeding client"
+
+Client.create(rooms:"two-bedroom", price:30000, location: "Ngara", img_url: "https://i.roamcdn.net/prop/brk/listing-thumb-376w/cb435f69849e64f83aee6349de932791/-/prod-property-core-backend-media-brk/4440829/023a05eb-34d0-417c-9742-96566adecde7.jpeg", contact: 0765342425)
+Client.create(rooms:"two-bedroom", price:20000, location: "Ngara", img_url: "https://i.roamcdn.net/prop/brk/listing-thumb-376w/cb435f69849e64f83aee6349de932791/-/prod-property-core-backend-media-brk/4440829/023a05eb-34d0-417c-9742-96566adecde7.jpeg", contact: 0765342425)
+Client.create(rooms:"two-bedroom", price:15000, location: "Ngara", img_url: "https://i.roamcdn.net/prop/brk/listing-thumb-376w/cb435f69849e64f83aee6349de932791/-/prod-property-core-backend-media-brk/4440829/023a05eb-34d0-417c-9742-96566adecde7.jpeg", contact: 0765342425)
+Client.create(rooms:"two-bedroom", price:10000, location: "Ngara", img_url: "https://i.roamcdn.net/prop/brk/listing-thumb-376w/cb435f69849e64f83aee6349de932791/-/prod-property-core-backend-media-brk/4440829/023a05eb-34d0-417c-9742-96566adecde7.jpeg", contact: 0765342425)
+
+puts "done seeding client"
+puts "seeding apartment"
+
+Apartment.create(rooms:"two-bedroom", price:60000, location: "Ngara", image_url: "https://i.roamcdn.net/prop/brk/listing-thumb-376w/cb435f69849e64f83aee6349de932791/-/prod-property-core-backend-media-brk/4440829/023a05eb-34d0-417c-9742-96566adecde7.jpeg", contact: 0765342425, client_id: 1, landlord_id: 1)
+Apartment.create(rooms:"two-bedroom", price:32000, location: "Ngara", image_url: "https://i.roamcdn.net/prop/brk/listing-thumb-376w/cb435f69849e64f83aee6349de932791/-/prod-property-core-backend-media-brk/4440829/023a05eb-34d0-417c-9742-96566adecde7.jpeg", contact: 0765342425, client_id: 1, landlord_id: 1)
+Apartment.create(rooms:"two-bedroom", price:45000, location: "Ngara", image_url: "https://i.roamcdn.net/prop/brk/listing-thumb-376w/cb435f69849e64f83aee6349de932791/-/prod-property-core-backend-media-brk/4440829/023a05eb-34d0-417c-9742-96566adecde7.jpeg", contact: 0765342425, client_id: 1, landlord_id: 1)
+Apartment.create(rooms:"two-bedroom", price:80000, location: "Ngara", image_url: "https://i.roamcdn.net/prop/brk/listing-thumb-376w/cb435f69849e64f83aee6349de932791/-/prod-property-core-backend-media-brk/4440829/023a05eb-34d0-417c-9742-96566adecde7.jpeg", contact: 0765342425, client_id: 1, landlord_id: 1)
+puts "done seeding apartment"
+
+puts "seeding landlord"
+Landlord.create(rooms:"two-bedroom", price:35000, location: "Ngara", img_url: "https://i.roamcdn.net/prop/brk/listing-thumb-376w/cb435f69849e64f83aee6349de932791/-/prod-property-core-backend-media-brk/4440829/023a05eb-34d0-417c-9742-96566adecde7.jpeg", contact: 0765342425)
+Landlord.create(rooms:"two-bedroom", price:90000, location: "Ngara", img_url: "https://i.roamcdn.net/prop/brk/listing-thumb-376w/cb435f69849e64f83aee6349de932791/-/prod-property-core-backend-media-brk/4440829/023a05eb-34d0-417c-9742-96566adecde7.jpeg", contact: 0765342425)
+Landlord.create(rooms:"two-bedroom", price:100000, location: "Ngara", img_url: "https://i.roamcdn.net/prop/brk/listing-thumb-376w/cb435f69849e64f83aee6349de932791/-/prod-property-core-backend-media-brk/4440829/023a05eb-34d0-417c-9742-96566adecde7.jpeg", contact: 0765342425)
+Landlord.create(rooms:"two-bedroom", price:65000, location: "Ngara", img_url: "https://i.roamcdn.net/prop/brk/listing-thumb-376w/cb435f69849e64f83aee6349de932791/-/prod-property-core-backend-media-brk/4440829/023a05eb-34d0-417c-9742-96566adecde7.jpeg", contact: 0765342425)
+puts "🌱 Done seeding!"
