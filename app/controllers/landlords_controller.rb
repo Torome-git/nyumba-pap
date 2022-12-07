f13042f85cf84144c473a82589f03ac0ae560bc7
soila.torome <soila.torome@student.moringaschool.com>
2022-12-07 11:27:43 +0300

    add routing for landlords

--- app/controllers/landlords_controller.rb
+++ app/controllers/landlords_controller.rb
@@ -1,11 +1,37 @@
 class LandlordsController < ApplicationController
+
+    def index
+        render json: Landlord.all
+    end
+
     def create
-        
+        landlord = Landlord.create(landlord_params)
+        render json: landlord
     end
 
     def update
+        landlord = Landlord.find_by(id: params[:id])
+        if landlord
+            landlord.update(landlord_params)
+            render json: landlord
+        else
+            render json: { error: "Apartment not found"}, status: :not_found
+        end
     end
 
     def destroy
+        landlord = Landlord.find_by(id: params[:id])
+        if landlord
+            landlord.destroy
+            head :no_content
+        else
+            render json: { error: "Apartment not found" }, status: :not_found
+        end
+    end
+
+    private
+
+    def landlord_params
+        params.permit(:rooms, :location, :price, :contact :img_url)
     end
 end
