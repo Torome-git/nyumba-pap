a32ff3be94765f15d256b92ab4d135a8b62e8dcd
soila.torome <soila.torome@student.moringaschool.com>
2022-12-09 16:54:53 +0300

    add sign up and login links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :apartments, only: [:index]
-  resources :client, only: [:index, :show] 
+  resources :client, only: [:index, :show, :create] 
   resources :landlords, only: [:index, :show, :create, :update, :destroy]
+  post "/signup", to: "sessions#create"
+  get "/me", to: "sessions#show"
 end
